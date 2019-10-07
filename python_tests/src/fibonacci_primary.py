2e385371baafb4fb4a9aad592c3e6fc7f4361637
paciadawid <paciadawid@gmail.com>
2019-10-07 16:56:44 +0200

    fibonacci primary

--- python_tests/src/fibonacci_primary.py
+++ python_tests/src/fibonacci_primary.py
@@ -12,13 +12,27 @@ def fibonacci(n_element):
             return a_2
         if n_element <= 0:
             return False
-        for _ in range(2, n_element + 1):
+
+        num_of_element = 0
+        while True:
+            if if_primary(a_2):
+                num_of_element += 1
             a_1, a_2 = a_2, a_1 + a_2
-        return a_1
+            if num_of_element == n_element:
+                return a_1
     except (TypeError, MemoryError) as err:
         print(err)
         return False
 
 
+def if_primary(value):
+    if value >= 2:
+        for i in range(2, value//2):
+            if value % i == 0:
+                return False
+        return True
+    return False
+
 if __name__ == "__main__":
     print(fibonacci(10))
+    print(if_primary(21))
