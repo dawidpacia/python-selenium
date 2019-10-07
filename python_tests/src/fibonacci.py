03d9eb33f44edd1a040948212028e946b0d2c613
paciadawid <paciadawid@gmail.com>
2019-10-07 16:25:28 +0200

    Fibonacci try/except

--- python_tests/src/fibonacci.py
+++ python_tests/src/fibonacci.py
@@ -1,14 +1,18 @@
 def fibonacci(n):
     a1, a2 = 0, 1
-    if n == 1:
+    try:
+        if n == 1:
+            return a1
+        elif n == 2:
+            return a2
+        elif n <= 0:
+            return False
+        for i in range(2, n + 1):
+            a1, a2 = a2, a1 + a2
         return a1
-    elif n == 2:
-        return a2
-    elif n <= 0:
+    except (TypeError, MemoryError) as e:
+        print(e)
         return False
-    for i in range(2, n + 1):
-        a1, a2 = a2, a1 + a2
-    return a1
 
 
 if __name__ == "__main__":
